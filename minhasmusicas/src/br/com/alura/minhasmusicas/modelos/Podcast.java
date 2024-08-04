1142e193d154b4b89800317b31f1dfac5c05ff6a
Paulo <phmartinsnetto@gmail.com>
2024-08-04 10:50:08 -0300

    Adicionando o projeto minhasmusicas

---
+++ minhasmusicas/src/br/com/alura/minhasmusicas/modelos/Podcast.java
@@ -0,0 +1,30 @@
+package br.com.alura.minhasmusicas.modelos;
+
+public class Podcast extends Audio {
+    private String apresentador;
+    private String descricao;
+
+    @Override
+    public int getClassificacao() {
+        if (this.getTotalCurtidas() > 500) {
+            return 10;
+        }
+        return 8;
+    }
+
+    public String getApresentador() {
+        return apresentador;
+    }
+    public void setApresentador(String apresentador) {
+        this.apresentador = apresentador;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+
+}
